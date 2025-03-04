307fb285f132d49e3856ba7b58b459aecbb0ea71
nifontovsv <nifontovxv@gmail.com>
2025-03-05 00:55:04 +0300

    Updates

--- src/component/Container/Container.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Container.module.scss';
-
-function Container(props) {
-	return <section className={styles.container}>{props.children}</section>;
-}
-
-export default Container;
